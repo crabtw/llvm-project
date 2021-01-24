5d6a7e6133fc0b071ed479bbb7aea21d13a0b342
Jyun-Yan You <jyyou.tw@gmail.com>
2021-01-24 15:45:04 +0800

    nix develop

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+      buildPackage = { system }:
+        let
+
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # deps
+            cmake
+            python3
+            libedit
+            libffi
+            libbfd
+            libxml2
+            ncurses
+            zlib
+            ninja
+
+            # host toolchain
+            llvmPackages_latest.libcxxClang
+            llvmPackages_latest.lld
+            gdb
+
+            # review
+            arcanist
+          ];
+        };
+
+    in {
+      devShell = forAllSystems (system:
+        buildPackage { inherit system; }
+      );
+    };
+}
